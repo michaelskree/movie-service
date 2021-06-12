1d02eeadcec19b0d0c44091a300e8191da051e65
Michael Skree <michael.skree@gmail.com>
2021-06-12 11:21:42 -0500

    Add route for updating a movie

--- src/main/java/com/michaelskree/movieservice/MovieServiceController.java
+++ src/main/java/com/michaelskree/movieservice/MovieServiceController.java
@@ -3,6 +3,7 @@ package com.michaelskree.movieservice;
 import java.util.UUID;
 
 import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -35,6 +37,17 @@ public class MovieServiceController {
     @PostMapping("/movie")
     public Movie createMovie(@RequestBody @Valid Movie movie) {
         return movieRepository.save(movie);
+    }
+
+    @PutMapping("/movie/{id}")
+    public void updateMovie(@PathVariable UUID id, @RequestBody @Valid Movie movie) {
+        if (movieRepository.existsById(id)) {
+            movie.id = id;
+            movieRepository.save(movie);
+            return;
+        }
+
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("/movie/{id}")
