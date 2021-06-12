89322fb7157c18d414165c0563318c4bd8cee2ea
Michael Skree <michael.skree@gmail.com>
2021-06-11 21:50:16 -0500

    Add route for deleting a movie

--- src/main/java/com/michaelskree/movieservice/MovieServiceController.java
+++ src/main/java/com/michaelskree/movieservice/MovieServiceController.java
@@ -1,14 +1,19 @@
 package com.michaelskree.movieservice;
 
-import javax.validation.Valid;
+import java.util.UUID;
 
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class MovieServiceController {
@@ -24,5 +29,15 @@ public class MovieServiceController {
     @PostMapping("/movie")
     public Movie createMovie(@RequestBody @Valid Movie movie) {
         return movieRepository.save(movie);
+    }
+
+    @DeleteMapping("/movie/{id}")
+    public void deleteMovie(@PathVariable UUID id) {
+        if (movieRepository.existsById(id)) {
+            movieRepository.deleteById(id);
+            return;
+        }
+
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
 }
